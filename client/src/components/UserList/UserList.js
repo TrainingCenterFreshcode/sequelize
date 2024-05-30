957fdeddff7668940d54cbc558630654ec299f5d
Roman Ratushnyi <roman.ratushnyi@freshcodeit.com>
2024-05-30 19:04:45 +0300

    Get users - done

--- client/src/components/UserList/UserList.js
+++ client/src/components/UserList/UserList.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { getUsers } from '../../api';
+import UserCard from './UserCard';
+import './style.css';
 
 const UserList = () => {
   const [users, setUsers] = useState([]);
@@ -20,15 +22,39 @@ const UserList = () => {
     });
   }
 
+  const renderUsers = () => {
+    return users.map((user) => <UserCard user={user} key={user.id} />);
+  }
+
+  const prevBtnHandler = () => {
+    if(page > 1) {
+      setPage(page - 1);
+    }
+  }
+
+  const nextBtnHandler = () => {
+    setPage(page + 1);
+  }
+
   useEffect(() => {
     loadUsers(page)
   }, [page]);
   // Пустий масив залежностей - componentDidMount
 
   return (
-    <div>
-      
-    </div>
+    <>
+      <h1>User List</h1>
+
+      {isLoading && <h2 className='loading'>Loading....</h2>}
+      <section className='card-container'>
+        {users.length > 0 ? renderUsers() : <h2 className='error'>Users not found</h2>}
+      </section>
+
+      <div>
+        <button onClick={prevBtnHandler} disabled={page === 1}>Previous page</button>
+        <button onClick={nextBtnHandler} disabled={users.length < 5}>Next page</button>
+      </div>
+    </>
   );
 }
 
